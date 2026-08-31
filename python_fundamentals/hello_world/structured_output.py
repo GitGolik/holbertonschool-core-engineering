eb8d4559dd06a34a5f3b1e15a2ed8029a0e84cba
GitGolik <goliktheo.pro@gmail.com>
2026-08-31 16:09:16 +0200

    fix: missing whitespace after ',' on 9 and 10

--- python_fundamentals/hello_world/structured_output.py
+++ python_fundamentals/hello_world/structured_output.py
@@ -3,8 +3,7 @@
 pi = 3.14
 comp = 3 > 2
 
-
 print("Language: Python")
 print("Version: 3")
-print("Pi approx:",pi)
-print("Computation valid:",comp)
+print("Pi approx:", pi)
+print("Computation valid:", comp)
