74d1eb56995cc0b62465a6935db35d333f3d9a93
GitGolik <goliktheo.pro@gmail.com>
2026-08-31 16:03:30 +0200

    feat: add variable comp = True

--- python_fundamentals/hello_world/structured_output.py
+++ python_fundamentals/hello_world/structured_output.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
 pi = 3.14
+comp = True
 
 
 print("Language: Python")
 print("Version: 3")
 print("Pi approx:",pi)
-print("Computation valid: True")
+print("Computation valid:"comp)
