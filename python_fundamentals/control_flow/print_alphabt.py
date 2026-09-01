f5b0e1724d57aa9b31c10c3c91f181dbe2d84020
GitGolik <goliktheo.pro@gmail.com>
2026-09-01 12:04:25 +0200

    FIX: add a .format and charge in an alphabet variable

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,3 +1,4 @@
 #!/usr/bin/env python3
 
-print("".join(chr(c) for c in range(ord('a'), ord ('z') + 1) if chr(c) not in ('q', 'e')))
+alphabet = "".join(chr(c) for c in range(ord('a'), ord ('z') + 1) if chr(c) not in ('q', 'e'))
+print("{}".format(alphabet))
