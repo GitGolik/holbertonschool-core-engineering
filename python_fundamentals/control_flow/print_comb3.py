1f4cc423296343510d5b14f722cef6ff89bf7359
GitGolik <goliktheo.pro@gmail.com>
2026-09-01 15:12:18 +0200

    FIX: remove a space line 8

--- python_fundamentals/control_flow/print_comb3.py
+++ python_fundamentals/control_flow/print_comb3.py
@@ -5,7 +5,7 @@ first = True
 for i in range(10):
     for j in range(i + 1, 10):
         if not first:
-            print(", ", end ="")
+            print(", ", end="")
         print("{:02d}".format(i * 10 + j), end="")
         first = False
 print()
