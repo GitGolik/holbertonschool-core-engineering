c71d4549fc77294bc2d0d8c0fa34b96892629208
GitGolik <goliktheo.pro@gmail.com>
2026-09-01 15:10:57 +0200

    FEAT: add print_comb3.py , print all smallest combination of number possible

---
+++ python_fundamentals/control_flow/print_comb3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+first = True
+
+for i in range(10):
+    for j in range(i + 1, 10):
+        if not first:
+            print(", ", end ="")
+        print("{:02d}".format(i * 10 + j), end="")
+        first = False
+print()
