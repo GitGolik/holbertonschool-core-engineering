2c1e99135e71f22b6abc2470325869c12f890794
GitGolik <goliktheo.pro@gmail.com>
2026-09-01 11:30:28 +0200

    FEAT: add last_digit.py , find the last digit

---
+++ python_fundamentals/control_flow/last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+number = __import__('random').randint(-10000, 10000)
+last = number % 10
+
+if last == 0:
+    print(f"Last digit of {number} is {last} and is 0")
+
+elif last > 5:
+    print(f"Last digit of {number} is {last} and is greater than 5")
+
+elif last < 6 && last != 0:
+    print(f"Last digit of {number} is {last} and is less than 6 and not 0")
