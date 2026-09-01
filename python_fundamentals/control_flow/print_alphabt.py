8841ec2487cf1a1785a2111f951344161a3629d1
GitGolik <goliktheo.pro@gmail.com>
2026-09-01 14:03:56 +0200

    FEAT: Mod function of how to print alphabt

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
 
-alphabet = "".join(chr(c) for c in range(ord('a'), ord ('z') + 1) if chr(c) not in ('q', 'e'))
-print("{}".format(alphabet))
+for chr in "abcdefghijklmnopqrstuvwxyz":
+    if chr != 'e' and chr != 'q':
+        print("{}".format(chr), end="")
