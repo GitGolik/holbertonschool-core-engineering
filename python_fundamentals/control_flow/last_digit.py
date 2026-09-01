fc0dcd9f4e09ee4b569cd437e426c4062fe427ec
GitGolik <goliktheo.pro@gmail.com>
2026-09-01 11:40:38 +0200

    FIX: change name variable digit -> last

--- python_fundamentals/control_flow/last_digit.py
+++ python_fundamentals/control_flow/last_digit.py
@@ -4,7 +4,7 @@ number = __import__('random').randint(-10000, 10000)
 last = number % 10
 
 if number < 0 and digit !=0:
-    digit -= 10
+    last -= 10
 
 if last == 0:
     print(f"Last digit of {number} is {last} and is 0")
