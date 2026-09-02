686c6cc215acb18b772601603421040e1c272d7d
GitGolik <goliktheo.pro@gmail.com>
2026-09-02 15:13:17 +0200

    FIX: cahnge result formul

--- python_fundamentals/functions_modules/pow.py
+++ python_fundamentals/functions_modules/pow.py
@@ -2,10 +2,12 @@
 
 def pow(a, b):
 
+    result = 0
+
     if b == 0:
-        result = 1
-        return result
+        result = result + 1
 
     for compt in range(b):
         result = result * a
-        return result
+
+    return result
