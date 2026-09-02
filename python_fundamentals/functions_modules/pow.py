f85e7f5795175a829018b83e1db3c88778809559
GitGolik <goliktheo.pro@gmail.com>
2026-09-02 15:16:23 +0200

    FIX: change formul if negative number 'b'

--- python_fundamentals/functions_modules/pow.py
+++ python_fundamentals/functions_modules/pow.py
@@ -4,6 +4,11 @@ def pow(a, b):
 
     result = 1
 
+    if b < 0:
+        for count in range(-b):
+            result = result * a
+        return 1 / result
+
     for compt in range(b):
         result = result * a
 
