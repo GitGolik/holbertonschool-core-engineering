b1e9e2e4782706b6737dcf2357cae574ddc2f3de
GitGolik <goliktheo.pro@gmail.com>
2026-09-02 14:16:12 +0200

    FEAT: add uppercase.py , check the str and change the character un lowercase to uppercase

---
+++ python_fundamentals/functions_modules/uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def uppercase(str):
+    for character in str:
+        code = ord(character)
+
+        if ord('a') <= code <= ord('z'):
+            character = chr(code - 32)
+
+        print(character, end="")
+    print()
