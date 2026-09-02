6221a294410cc7d5713a62001da4c25cb6eb5933
GitGolik <goliktheo.pro@gmail.com>
2026-09-02 15:03:34 +0200

    FIX: change the place of if

--- python_fundamentals/functions_modules/pow.py
+++ python_fundamentals/functions_modules/pow.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 
 def pow(a, b):
+    if b == 0:
+        result = 1
 
-    for compt in range(b):
-        if b == 0:
-            result = 1
-        else:
+        for compt in range(b):
             result = result * a
 
     return result
