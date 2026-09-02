bb6f48f08a4a80c08618249b960edc7dd60cb988
GitGolik <goliktheo.pro@gmail.com>
2026-09-02 15:04:40 +0200

    FIX: change indentation of the boucle for

--- python_fundamentals/functions_modules/pow.py
+++ python_fundamentals/functions_modules/pow.py
@@ -4,7 +4,7 @@ def pow(a, b):
     if b == 0:
         result = 1
 
-        for compt in range(b):
-            result = result * a
+    for compt in range(b):
+        result = result * a
 
     return result
