f66800100da034eb8d77e396392235cce9e62ad9
GitGolik <goliktheo.pro@gmail.com>
2026-09-03 14:52:48 +0200

    FIX: missing whitespace line 4 after return

--- python_fundamentals/core_data_structures/main.py
+++ python_fundamentals/core_data_structures/main.py
@@ -2,7 +2,6 @@
 
 common_elements = __import__('common_elements').common_elements
 
-
 set_1 = {"Python", "C", "Javascript"}
 set_2 = {"Bash", "C", "Ruby", "Perl"}
 print(sorted(list(common_elements(set_1, set_2))))
