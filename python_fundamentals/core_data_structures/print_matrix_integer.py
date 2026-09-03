aeb1e2740f3a44c369d222cc0077d5d44c0c9ad5
GitGolik <goliktheo.pro@gmail.com>
2026-09-03 13:05:08 +0200

    FIX: change script to adapt to no use of str and use of .format

--- python_fundamentals/core_data_structures/print_matrix_integer.py
+++ python_fundamentals/core_data_structures/print_matrix_integer.py
@@ -2,4 +2,12 @@
 
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        print(" ".join(str(x) for x in row))
+        if not row:
+            print()
+            continue
+        line = ""
+        for i, val in enumerate(row):
+            line += "{:d}".format(val)
+            if i < len(row) - 1:
+                line += " "
+        print(line)
