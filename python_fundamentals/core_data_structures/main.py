70d606e2c50c7ef4e8cbee509cecb9b563980ec9
GitGolik <goliktheo.pro@gmail.com>
2026-09-03 12:58:17 +0200

    FEAT: add print_matrix_integer.py , print a matrix of int row by row with a space between int. add main.py , exemple of main

--- python_fundamentals/core_data_structures/main.py
+++ python_fundamentals/core_data_structures/main.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
-replace_in_list = __import__('replace_in_list').replace_in_list
+print_matrix_integer = __import__('print_matrix_integer').print_matrix_integer
 
 
-my_list = [1, 2, 3, 4, 5]
-print(replace_in_list(my_list, 3, 99))
-print(replace_in_list(my_list, 2, 9))
-print(replace_in_list(my_list, 5, 43))
-print(replace_in_list(my_list, 0, 234))
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+print_matrix_integer(matrix)
