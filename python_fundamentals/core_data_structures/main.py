c4b13f21c9e47138f3bd6ee98e0ee4fd2ba58e91
GitGolik <goliktheo.pro@gmail.com>
2026-09-03 11:47:10 +0200

    FEAT: add element_at.py , print character of the list and none if out of range , add main.py exemple of main

--- python_fundamentals/core_data_structures/main.py
+++ python_fundamentals/core_data_structures/main.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 
-print_list_integer = __import__('print_list_integer').print_list_integer
+element_at = __import__('element_at').element_at
 
-my_list = [1, 2, 3, 4, 5]
-print_list_integer(my_list)
+my_list = ["a", "b", "c", "d", "e"]
+print(element_at(my_list, 3))
+print(element_at(my_list, 0))
+print(element_at(my_list, 4))
+print(element_at(my_list, 5))
+print(element_at(my_list, -14))
+print(element_at(my_list, 32))
+print(element_at(my_list, 15))
