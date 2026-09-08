8dd1facb1c2ca07e5c59686d28a0bb4adc790a3a
GitGolik <goliktheo.pro@gmail.com>
2026-09-08 14:18:28 +0200

    FIX: add @property and @size.sitter for getter and setter, and change some size by value for setter

--- python_oop/classes_and_object_model/4-square.py
+++ python_oop/classes_and_object_model/4-square.py
@@ -11,20 +11,25 @@ class Square:
         args:
             size of type int: size of the square
         """
-        self.__size = size
+        self.size = size
 
-        if not isinstance(size, int):
+    @property
+    def size(self) -> int:
+        """return the size attribute"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
 
-        if size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
 
-        self.__size = size
+        self.__size = value
 
     def area(self):
         """return area of a square"""
         return self.__size ** 2
 
-    def size(self) -> int:
-        """return the size attribute"""
-        return self.__size
