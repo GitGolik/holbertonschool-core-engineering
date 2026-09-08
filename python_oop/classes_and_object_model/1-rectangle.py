cf2bf29a23d50497585d2740367267362724831e
GitGolik <goliktheo.pro@gmail.com>
2026-09-08 16:24:28 +0200

    FEAT: add 1-rectangle.py, create the class rectangle and all attributes

---
+++ python_oop/classes_and_object_model/1-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""shibang python module"""
+
+class Rectangle:
+    """a ractangle with width and height attributes"""
+
+    def __init__(self, width=0, height=0):
+        """initialise a rectangle"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """get width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """set the width after validation"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """get height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """set the height after validation"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
