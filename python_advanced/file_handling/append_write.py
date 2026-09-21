96af77edbf0b0cf47f1f3714485631d124689366
GitGolik <goliktheo.pro@gmail.com>
2026-09-21 11:27:41 +0200

    FEAT: add append_wirte.py , add text into a file and return the number of char added

---
+++ python_advanced/file_handling/append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""append text into a file"""
+
+def append_write(filename="", text=""):
+    """append text into a file and return number of char added"""
+
+    with open(filename, "a", encoding="utf-8") as file:
+        return file.write(text)
